3ee580dabf57ae66844e4b9245410c8868f01460
Matthew Oliver <matthew.oliver1502@gmail.com>
2021-07-13 00:15:51 -0700

    first commit

---
+++ listScript.js
@@ -0,0 +1,28 @@
+let toDo = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+
+toDo.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+
+
+function addItem(e){
+    e.preventDefault();
+    let newToDo = document.getElementById('item').value;
+    let newLi = document.createElement("li");
+    newLi.className = 'list-group-item';
+    newLi.appendChild(document.createTextNode(newToDo));
+    itemList.appendChild(newLi);
+    let input = document.querySelector('input');
+    input.value = "";
+}
+
+
+function removeItem(e){
+    let text = e.target.innerText;
+    e.target.innerHTML = '<del>'+text+'</del>';
+    let timeout = setTimeout(delayedDelete, 1000);
+    function delayedDelete (){
+        itemList.removeChild(e.target);
+    }
+
+}
